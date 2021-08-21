637a21e0fcb744458c054cb0a4da90fba2289013
eude313 <eudes.charles@student.moringaschool.com>
2021-08-22 02:12:00 +0300

    add initial changes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,1 +1,15 @@
+from flask_wtf import Flask_form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, Email, DataRequired, EqualTo
 
+class registrationForm( Flask_form):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=6)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=8)])
+    confirm_password = PasswordField('confirm_password', validators=[Length(min=5, max=8), DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class loginForm( Flask_form):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=8)])
+    submit = SubmitField('Sign Up')
