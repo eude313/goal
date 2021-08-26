9984323adc1536602e190b29078c00f57c6652c3
eude313 <eudes.charles@student.moringaschool.com>
2021-08-26 16:31:16 +0300

    add initial changes

--- webapp/models.py
+++ webapp/models.py
@@ -21,7 +21,7 @@ class User( db.Model,UserMixin ):
     
     #how the object is printed when printed out
     def __repr__(self):
-        return f"User"('{self.username}','{self.email}','{self.image_file}')
+        return f"User"({self.username},{self.email},{self.image_file})
     
 class Post( db.Model ):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,7 +30,7 @@ class Post( db.Model ):
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     def __repr__(self):
-        return f"Post"('{self.title}','{self.date_posted}')
+        return f"Post"({self.title})
     
 
 
